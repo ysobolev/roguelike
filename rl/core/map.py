4c2897822517b2ebae6e8e5c64c052f292dcd269
Yury Sobolev <yury.sobolev@gmail.com>
2016-05-29 06:40:26 -0400

    Remove duplicate code.

--- rl/core/map.py
+++ rl/core/map.py
@@ -17,31 +17,6 @@ class Map:
             return tileset.ImpassableTile()
         return self.tiles[position[0]][position[1]]
 
-    def maze_fill(self):
-        return
-        graph = {}
-        for i in range(1, 21, 2):
-            for j in range(1, 79, 2):
-                if isinstance(self.tiles[i][j], tileset.WallTile):
-                    graph[(i, j)] = []
-        for tile in graph:
-            i, j = tile
-            if (i+2, j) in graph:
-                graph[(i, j)].append(((i+2, j), random.randint(1,5)))
-                graph[(i+2, j)].append(((i, j), random.randint(1,5)))
-            if (i, j+2) in graph:
-                graph[(i, j)].append(((i, j+2), random.randint(1,5)))
-                graph[(i, j+2)].append(((i, j), random.randint(1,5)))
-        tree = find_minimal_spanning_tree(graph, (5, 5))
-        for vertex in tree:
-            i1, j1 = vertex
-            if tree[vertex] is None:
-                self.tiles[i1][j1] = tileset.FloorTile()
-                continue
-            i2, j2 = tree[vertex]
-            self.tiles[i2][j2] = tileset.FloorTile()
-            self.tiles[(i1+i2)/2][(j1+j2)/2] = tileset.FloorTile()
-
 class Direction:
     compass = {"n":(-1, 0), "s":(1, 0), "w":(0, -1), "e":(0, 1),
         "nw":(-1, -1), "ne":(-1, 1), "sw":(1, -1), "se":(1, 1)}
